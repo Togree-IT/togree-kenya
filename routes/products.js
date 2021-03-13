c38fc7e09e5e894e2b5e957173bd0c7e0d234f3e
Zikama <nehemiezikama@gmail.com>
2021-03-13 16:16:10 +0300

    fixing host for domain

--- routes/products.js
+++ routes/products.js
@@ -150,7 +150,7 @@ router.get('/search', (req, res) => {
 router.get('/main/:id', (req, res) => {
     // chrome only
     // let broswer = req.headers['sec-ch-ua'].split(',')[1].split(';')[0]
-    console.log(req.hostname);
+    console.log(req.subdomains);
 
 
     require('../functions').destroy();
